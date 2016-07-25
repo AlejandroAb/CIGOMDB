1acc1e7f7eae542ad8cbb0b3edc12424d32c1c38
Alejandro <Alejandro@Opeth>
2016-07-25 12:58:55 -0500

    Primera versión creada, objeto básico

---
+++ src/bobjects/Usuario.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bobjects;
+
+/**
+ *
+ * @author Alejandro
+ */
+public class Usuario {
+
+    private int idUsuario;
+    private int idInstitucion;
+    private String nombres;
+    private String apellidos;
+    private String correo;
+    private String cite;
+    //wild keys para otras relaciones
+    private String acciones; //como ser wildkey para muestreo usuario
+    private String comentarios;
+    public Usuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getAcciones() {
+        return acciones;
+    }
+
+    public void setAcciones(String acciones) {
+        this.acciones = acciones;
+    }
+
+    public String getComentarios() {
+        return comentarios;
+    }
+
+    public void setComentarios(String comentarios) {
+        this.comentarios = comentarios;
+    }
+
+    
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public int getIdInstitucion() {
+        return idInstitucion;
+    }
+
+    public void setIdInstitucion(int idInstitucion) {
+        this.idInstitucion = idInstitucion;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getCite() {
+        return cite;
+    }
+
+    public void setCite(String cite) {
+        this.cite = cite;
+    }
+    
+}
